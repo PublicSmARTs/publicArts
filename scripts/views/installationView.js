d0dce2d56dd38fe1458f495e4180939fb445d738
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-10 14:30:39 -0800

    started render function

--- scripts/views/installationView.js
+++ scripts/views/installationView.js
@@ -3,6 +3,11 @@
 
   var installationView = {};
 
+  var render = function(installation) {
+    var template = Handlebars.compile($('#installation-template').text());
+    
+  }
+
   installationView.initNewInstallationPage = function() {
 
   };
@@ -10,19 +15,29 @@
 
   installationView.create = function() {
     var formInstallation;
+    $('#display-installation').empty();
+    console.log($('#new-installation'));
+    $('#new-installation').on('click', 'button', function(e){
+      e.preventDefault();
+      console.log('anything');
+      formInstallation = new Installation({
+        location: $('#installation-location').val(),
+        medium: $('#installation-medium').val(),
+        artist: $('#installation-artist').val() || 'UNKNOWN',
+        title: $('#installation-title').val() || 'UNKNOWN',
+        dateRange: $('#installation-date-range').val() || 'UNKNOWN',
+        // sanctioned: $('#installation-sanctioned')
+        // description: $('#installation-description')
+        // imageUrl: $('#installation-image-url')
+      });
+      Installation.insertRecord(formInstallation);
+      e.delegateTarget.reset();
+      //redirect here
+      //loveController.reveal();
+      $('#display-installation').append(render(formArticle));
 
-    formInstallation = new Installation({
-      location: $('#installation-location').val(),
-      medium: $('#installation-medium').val(),
-      artist: $('#installation-artist').val(),
-      title: $('#installation-title').val(),
-      dateRange: $('#installation-date-range').val(),
-      // sanctioned: $('#installation-sanctioned')
-      // description: $('#installation-description')
-      // imageUrl: $('#installation-image-url')
     });
 
-    console.log(formInstallation);
   };
 
   module.installationView = installationView;
