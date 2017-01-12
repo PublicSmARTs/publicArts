7488db38a6cabcd48b00189cd8d9838303e49a1b
William Madden <madden.william.d@gmail.com>
2017-01-11 22:13:40 -0800

    some refactoring

--- scripts/models/map.js
+++ scripts/models/map.js
@@ -1,24 +1,17 @@
 'use strict';
 
-// function initMap() { //creates map
-//   //var lat = 45.5315;
-//   //var lng = -122.6668;
-//   var mapOptions = {
-//     zoom: 7,
-//     center: {lat: 45.5231, lng: -122.6765},//new google.maps.LatLng(lat,lng),
-//     mapTypeId: google.maps.MapTypeId.ROADMAP,
-//     fullscreenControl: true
-//   };
-//
-//   // create the map and reference the div#map-canvas container
-//   var map = new google.maps.Map(document.getElementById('findMap'), mapOptions);
-// };
-initMap();
-function initMap () {
-  var findMap = new google.maps.Map(document.getElementById('findMap'), {
+function initMap() { //creates map
+  var lat = 45.5315;
+  var lng = -122.6668;
+  var mapOptions = {
     zoom: 10,
-    center: {lat: 45.5231, lng: -122.6765},
-    mapTypeId: 'roadmap',
-    fullscreenControl: true
-  });
+    center: {lat, lng},//new google.maps.LatLng(lat,lng),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+
+  };
+
+  var map = new google.maps.Map(document.getElementById('findMap'), mapOptions);
 };
+
+
+initMap();
