3ef5c369197a9b5c7353d7a11868ad79bc8502df
William Madden <madden.william.d@gmail.com>
2017-01-12 00:01:31 -0800

    maps maps and more maps. new map model created, places map in chare art page, findmap model places map in find art page, auto compete works and has access to google places library, click even listener not working for some reason,but its mdnight and im going to bed peace!

---
+++ scripts/models/artMap.js
@@ -0,0 +1,25 @@
+'use strict';
+(function(module) {
+
+var lat = 45.5315;
+var lng = -122.6668;
+
+
+artMap.initMap = function () { //creates map
+
+  var mapOptions = {
+    zoom: 10,
+    center: {lat, lng},//new google.maps.LatLng(lat,lng),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
+  var map = new google.maps.Map(document.getElementById('artMap'), mapOptions);
+  add_autoComplete();
+};
+artMap.initMap();
+
+
+
+
+module.artMap = artMap;
+
+})(window);
