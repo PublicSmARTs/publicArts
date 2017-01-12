e474c1f9bebba25ae22fb09775c0e453f7a6590d
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-12 14:20:26 -0800

    testing filters

--- scripts/controllers/installationsController.js
+++ scripts/controllers/installationsController.js
@@ -4,9 +4,9 @@
   Installation.createTable();
   var installationsController = {};
 
-  installationsController.index = function(ctx, next) {
-    searchView.index(ctx.installation);
-  };
+  // installationsController.index = function(ctx, next) {
+  //   searchView.index(ctx.installation);
+  // };
 
   //load by all filters
   installationsController.loadByMedium = function(ctx, next) {
@@ -19,19 +19,19 @@
 
   //MOCK MIDDLEWARE TO BE DELETED
 
-  searchView.index = function(installations) {
-    $('#findArt').show().siblings().hide();
-
-    $('').remove(); //will need to clear out past map render when we know how
-
-    installations.forEach(function(art) {
-      $('#findArt').append(render(art));
-    }); //need function to add pin to map here
-
-    searchView.populateFilters();
-    searchView.handleFilters();
-
-  };
+  // searchView.index = function(installations) {
+  //   $('#findArt').show().siblings().hide();
+  //
+  //   $('').remove(); //will need to clear out past map render when we know how
+  //
+  //   installations.forEach(function(art) {
+  //     $('#findArt').append(render(art));
+  //   }); //need function to add pin to map here
+  //
+  //   searchView.populateFilters();
+  //   searchView.handleFilters();
+  //
+  // };
 
   module.installationsController = installationsController;
 
--- scripts/views/searchView.js
+++ scripts/views/searchView.js
@@ -35,7 +35,24 @@
     }); //installation.allTitles
   };//populateFilters
 
+  searchView.handleFilters = function () {
+    $('#filters').one('change', 'select', function() {
+      var f = this.id.replace('-filter', '');
+      var v = $(this).val();
+      console.log(f);
+      console.log(v);
+      Installation.findWhere(f, v, temp);
+    });
+  };
+
+  function temp(test) {
+    console.log(test);
+  }
+  //inside callback function - clear the map and redraw it, that function will be passed data
+  //two things:
+  //when the filters changed, we need to get info about how they changed
+
   searchView.populateFilters();
-  //searchView.handleFilters();
+  searchView.handleFilters();
   module.searchView = searchView;
 })(window);
