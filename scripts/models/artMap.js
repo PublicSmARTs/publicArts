ea726a076f38ea1ac05abbb808806b1aa13fecf3
William Madden <madden.william.d@gmail.com>
2017-01-12 10:49:21 -0800

    errors fixed, find art search bar accepting and returning data as expected

--- scripts/models/artMap.js
+++ scripts/models/artMap.js
@@ -5,19 +5,19 @@ var lat = 45.5315;
 var lng = -122.6668;
 
 
-artMap.initMap = function () { //creates map
-
-  var mapOptions = {
-    zoom: 10,
-    center: {lat, lng},//new google.maps.LatLng(lat,lng),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-  };
-  var map = new google.maps.Map(document.getElementById('artMap'), mapOptions);
-  add_autoComplete();
-};
-artMap.initMap();
-
-
+// artMap.initMap = function () { //creates map
+//
+//   var mapOptions = {
+//     zoom: 10,
+//     center: {lat, lng},//new google.maps.LatLng(lat,lng),
+//     mapTypeId: google.maps.MapTypeId.ROADMAP,
+//   };
+//   var map = new google.maps.Map(document.getElementById('artMap'), mapOptions);
+//   add_autoComplete();
+// };
+// artMap.initMap();
+//
+//
 
 
 module.artMap = artMap;
