479005bc9ce2c61467a7b8d921474657809345d5
William Madden <madden.william.d@gmail.com>
2017-01-11 23:48:48 -0800

    trouble with getting location : html works to get lat/lng but i dont know how to access the data beyond logging it , I have gotten the google geocoder to work which gives us the ability to later turn any address into a set of coordinates, however we need an address to start with and I dont know how to get the address of where the user is, just the lat/lng, its a matter of figuring out how to get a object to return to a place it can be manipulated. I have the auto complete working and a sarch bar added to the find, map , css needs to ba adjusted. going to add another model for the next map and then call it a night

--- scripts/models/map.js
+++ scripts/models/map.js
@@ -1,11 +1,9 @@
 'use strict';
 (function(module) {
-var findMap ={};
 
 var lat = 45.5315;
 var lng = -122.6668;
-
-findMap.getLocation = function () {
+function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
@@ -13,31 +11,57 @@ findMap.getLocation = function () {
     }
 }
 
-findMap.showPosition = function (position) {
+function showPosition(position) {
   var returning = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
     console.log(returning);
 }
- findMap.getCenter = function (postion){
-  var cntrLat = position.coords.latitude;
-  var cntrLng = position.coords.longitude;
-  cntrCoordObj = {lat : cntrLat , lng : cntrLng};
-  return cntrCoordObj;
-}
-
-findMap.setCenter =  function (){
-navigator.geolocation.getCurrentPosition(showPosition);
-}
 
 findMap.initMap = function () { //creates map
+
   var mapOptions = {
     zoom: 10,
     center: {lat, lng},//new google.maps.LatLng(lat,lng),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
   };
   var map = new google.maps.Map(document.getElementById('findMap'), mapOptions);
+  add_autoComplete();
 };
 findMap.initMap();
+
+
+/* SOURCED from google tutorial@: http://gmap-tutorial-101.appspot.com/mapsapi101/toc*/
+function add_autoComplete(){
+  var acOptions = {
+    types: ['address']
+  };
+  var autocomplete = new google.maps.places.Autocomplete(document.getElementById('ac'),acOptions);
+  autocomplete.bindTo('bounds',map);
+  var infoWindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map
+  });
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    infoWindow.close();
+    var place = autocomplete.getPlace();
+    console.log(place.geometry.location);
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+    marker.setPosition(place.geometry.location);
+    infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
+    infoWindow.open(map, marker);
+    google.maps.event.addListener(marker,'click',function(e){
+
+      infoWindow.open(map, marker);
+
+    });
+  });
+}
 
 module.findMap = findMap;
 
