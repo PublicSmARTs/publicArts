9a10e3e519541c4d918c98a3911ba0701a5cb5b8
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-11 20:17:49 -0800

    server changes and managed conflicts

--- appApiKey.js
+++ appApiKey.js
@@ -1,3 +1,2 @@
-
 'use strict';
 var apiKey = 'AIzaSyAWLK7zwl3GRbE1VqeFD3bA7EmwFxFLyks';
--- scripts/controllers/installationsController.js
+++ scripts/controllers/installationsController.js
@@ -4,9 +4,9 @@
   Installation.createTable();
   var installationsController = {};
 
-  installationsController.index = function(ctx, next) {
-    searchView.index(ctx.installation);
-  };
+  // installationsController.index = function(ctx, next) {
+  //   searchView.index(ctx.installation);
+  // };
 
   //load by all filters
   installationsController.loadByMedium = function(ctx, next) {
@@ -21,8 +21,8 @@
 
   //MOCK MIDDLEWARE TO BE DELETED
 
-  searchView.index = function(installations) {
-    $('#findArt').show().siblings().hide();
+  // searchView.index = function(installations) {
+  //   $('#findArt').show().siblings().hide();
 
     //$('').remove(); will need to clear out past map render when we know how
 
@@ -30,10 +30,10 @@
     //   $('#findArt').append(render(art));
     // }); need function to add pin to map here
 
-    searchView.populateFilters();
+    // searchView.populateFilters();
     //searchView.handleFilters();
 
-  };
+  // };
 
   module.installationsController = installationsController;
 
