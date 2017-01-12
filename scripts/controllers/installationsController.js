84ffa134e399c2f66976325772df6a142b7e279d
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-12 13:07:39 -0800

    started to work on routing

--- scripts/controllers/installationsController.js
+++ scripts/controllers/installationsController.js
@@ -4,9 +4,9 @@
   Installation.createTable();
   var installationsController = {};
 
-  // installationsController.index = function(ctx, next) {
-  //   searchView.index(ctx.installation);
-  // };
+  installationsController.index = function(ctx, next) {
+    searchView.index(ctx.installation);
+  };
 
   //load by all filters
   installationsController.loadByMedium = function(ctx, next) {
@@ -15,24 +15,23 @@
       next();
     };
     Installation.findWhere('medium', ctx.params.mediumName.replace('+', ' '), mediumData);
-      console.log('mediumData ', mediumData);
   };
 
   //MOCK MIDDLEWARE TO BE DELETED
 
-  // searchView.index = function(installations) {
-  //   $('#findArt').show().siblings().hide();
+  searchView.index = function(installations) {
+    $('#findArt').show().siblings().hide();
 
-    //$('').remove(); will need to clear out past map render when we know how
+    $('').remove(); //will need to clear out past map render when we know how
 
-    // installations.forEach(function(art) {
-    //   $('#findArt').append(render(art));
-    // }); need function to add pin to map here
+    installations.forEach(function(art) {
+      $('#findArt').append(render(art));
+    }); //need function to add pin to map here
 
-    // searchView.populateFilters();
-    //searchView.handleFilters();
+    searchView.populateFilters();
+    searchView.handleFilters();
 
-  // };
+  };
 
   module.installationsController = installationsController;
 
