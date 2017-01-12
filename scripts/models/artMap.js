4c7b44f8cb611036fd8a42c2ec8ac3bb87d513e0
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-12 11:41:16 -0800

    got shareMap to drop the pin

--- scripts/models/artMap.js
+++ scripts/models/artMap.js
@@ -1,24 +1,71 @@
 'use strict';
 (function(module) {
 
-var lat = 45.5315;
-var lng = -122.6668;
+;
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log( "Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+  var returning = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+    console.log(returning);
+}
+
+artMap.initMap = function () { //creates map
+  var lat = 45.5315;
+  var lng = -122.6668
+  var mapOptions = {
+    zoom: 10,
+    center: {lat , lng},//new google.maps.LatLng(lat,lng),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
+  var map = new google.maps.Map(document.getElementById('artMap'), mapOptions);
+  add_autoComplete(map);
+};
+artMap.initMap();
 
 
-// artMap.initMap = function () { //creates map
-//
-//   var mapOptions = {
-//     zoom: 10,
-//     center: {lat, lng},//new google.maps.LatLng(lat,lng),
-//     mapTypeId: google.maps.MapTypeId.ROADMAP,
-//   };
-//   var map = new google.maps.Map(document.getElementById('artMap'), mapOptions);
-//   add_autoComplete();
-// };
-// artMap.initMap();
-//
-//
+/* SOURCED from google tutorial@: http://gmap-tutorial-101.appspot.com/mapsapi101/toc*/
+function add_autoComplete(map){
+  var acOptions = {
+    types: ['address']
+  };
+  var autocomplete = new google.maps.places.Autocomplete(document.getElementById('ac-2'),acOptions);
+  autocomplete.bindTo('bounds',map);
+  var infoWindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map
+  });
+  console.log('yo');
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    infoWindow.close();
+    var place = autocomplete.getPlace();
+    console.log(place.geometry.location);
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+    marker.setPosition(place.geometry.location);
 
+    infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
+    infoWindow.open(map, marker);
+
+
+    google.maps.event.addListener(marker,'click',function(e){
+
+      infoWindow.open(map, marker);
+
+    });
+  });
+
+}
 
 module.artMap = artMap;
 
