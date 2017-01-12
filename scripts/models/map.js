9bf0a4cf719d418ec149e6d0b4cf1d90d3d5e85e
William Madden <madden.william.d@gmail.com>
2017-01-11 22:15:40 -0800

    -ive been committing these changes on the wrong branch!

--- scripts/models/map.js
+++ scripts/models/map.js
@@ -1,5 +1,19 @@
 'use strict';
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log( "Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+  var returning = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+    console.log(returning);
+}
+
 function initMap() { //creates map
   var lat = 45.5315;
   var lng = -122.6668;
@@ -7,11 +21,7 @@ function initMap() { //creates map
     zoom: 10,
     center: {lat, lng},//new google.maps.LatLng(lat,lng),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-
   };
-
   var map = new google.maps.Map(document.getElementById('findMap'), mapOptions);
 };
-
-
 initMap();
