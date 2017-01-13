1189b239a72e28b712fc2a340e53810cf2b0c2d2
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-13 11:03:04 -0800

    adjusted images

--- scripts/controllers/installationsController.js
+++ scripts/controllers/installationsController.js
@@ -8,7 +8,6 @@
     installationView.index(ctx.installation);
   };
 
-  //load by all filters
   installationsController.loadByMedium = function(ctx, next) {
     var mediumData = function(installationsInMedium) {
       ctx.installation = installationsInMedium;
@@ -16,22 +15,6 @@
     };
     Installation.findWhere('medium', ctx.params.mediumName.replace('+', ' '), mediumData);
   };
-
-  //MOCK MIDDLEWARE TO BE DELETED
-
-  // searchView.index = function(installations) {
-  //   $('#findArt').show().siblings().hide();
-  //
-  //   $('').remove(); //will need to clear out past map render when we know how
-  //
-  //   installations.forEach(function(art) {
-  //     $('#findArt').append(render(art));
-  //   }); //need function to add pin to map here
-  //
-  //   searchView.populateFilters();
-  //   searchView.handleFilters();
-  //
-  // };
 
   module.installationsController = installationsController;
 
